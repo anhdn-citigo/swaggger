0e08980aacae3db8479864b30256e266ca6c43ab
anhdn <anhdn611@hotmail.com>
2023-03-17 07:08:51 +0700

    update proj

--- RMA Mobile API/Todo.cs
+++ RMA Mobile API/Todo.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿
+using System.ComponentModel;
+using Swashbuckle.AspNetCore.Annotations;
+
+[SwaggerSchema(Required = new[] { "Response trả về" })]
 
 public class Todo
 {
+    [SwaggerSchema("The identifier", ReadOnly = true)]
     public int Id { get; set; }
+
+    [SwaggerSchema("Tên công việc muốn làm")]
     public string? Name { get; set; }
+
+    [DefaultValue(false), SwaggerSchema("Trạng thái công việc  đã hoàn thành")]
     public bool IsComplete { get; set; }
 }
